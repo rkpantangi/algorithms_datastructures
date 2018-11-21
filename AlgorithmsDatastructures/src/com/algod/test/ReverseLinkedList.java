9b32602f6b3c99673b8ecaa4eb77149500c01d57
Ram Pantangi <ram.pantangi@gmail.com>
2018-11-20 22:30:32 -0500

    Update

--- AlgorithmsDatastructures/src/com/algod/test/ReverseLinkedList.java
+++ AlgorithmsDatastructures/src/com/algod/test/ReverseLinkedList.java
@@ -10,27 +10,27 @@ public class ReverseLinkedList {
 //		Node n2 = new Node(2, n3);
 //		Node n1 = new Node(1, n2);
 		
-		LinkedNode n1 = new LinkedNode(1);
-		n1.next(new LinkedNode(2)).next(new LinkedNode(3)).next(new LinkedNode(4));
+		LinkedNode<Integer> n1 = new LinkedNode<>(1);
+		n1.next(new LinkedNode<>(2)).next(new LinkedNode<>(3)).next(new LinkedNode<>(4));
 
 		printList(n1);
 		
-		LinkedNode nhead = reverse(n1);
+		LinkedNode<Integer> nhead = reverse(n1);
 		
 		printList(nhead);
 	}
 	
-	private static void printList(LinkedNode node) {
+	private static void printList(LinkedNode<Integer> node) {
 		if (node == null) return;
 		System.out.println(node.val);
 		printList(node.next);
 	}
 	
-	private static LinkedNode reverse(LinkedNode head) {
-		LinkedNode current = head; // 1
-		LinkedNode prev = null; // null
+	private static LinkedNode<Integer> reverse(LinkedNode<Integer> head) {
+		LinkedNode<Integer> current = head; // 1
+		LinkedNode<Integer> prev = null; // null
 		while(true) {
-			LinkedNode next = current.next; // 2
+			LinkedNode<Integer> next = current.next; // 2
 			current.next = prev; // 2
 			prev = current; 
 			if (next == null) break;
